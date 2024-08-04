5e30dfca47381481d56148141b1fabe5f2aeddb0
ana valencia <avalenciasolano1@gmail.com>
2024-08-03 22:56:07 -0500

    creación proyecto en Django y + config.

---
+++ backend/Ferrementum/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Ferrementum project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ferrementum.settings')
+
+application = get_wsgi_application()
