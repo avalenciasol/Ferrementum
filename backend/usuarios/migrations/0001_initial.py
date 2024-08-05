b2127c1d9c2970498649ff68743e9cb1d1865358
ana valencia <avalenciasolano1@gmail.com>
2024-08-04 22:00:41 -0500

    config. migración y actualización de requirements.txt

---
+++ backend/usuarios/migrations/0001_initial.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = []
+
+    operations = [
+        # Agregar aquí las operaciones que necesites manualmente, si las hay
+    ]
