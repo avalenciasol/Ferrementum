66924bfaa778fac9c6945c6e28676f941e5a05e8
ana valencia <avalenciasolano1@gmail.com>
2024-09-09 21:57:05 -0500

    backend config. 4

--- backend/usuarios/serializers/usuarioSerializer.py
+++ backend/usuarios/serializers/usuarioSerializer.py
@@ -1,23 +1,23 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-Usuario = get_user_model()
+User = get_user_model()
 
 class UsuarioSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = Usuario
-        fields = ('id', 'doc', 'tipo_doc', 'email', 'nombre', 'apellido','telefono', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        model = User
+        fields = ('id', 'doc', 'tipo_doc', 'email', 'nombre', 'apellido', 'telefono', 'password')
+        extra_kwargs = {'password': {'write_only': True}} 
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        usuario = super().create(validated_data)
+        user = super().create(validated_data)
         if password:
-            usuario.set_password(password)
-            usuario.save()
-        return usuario
+            user.set_password(password)
+            user.save()
+        return user
     
     def update(self, instance, validated_data):
         instance.nombre = validated_data.get('nombre', instance.nombre)
@@ -25,7 +25,7 @@ class UsuarioSerializer(serializers.ModelSerializer):
         instance.telefono = validated_data.get('telefono', instance.telefono)
 
         password = validated_data.get('password')
-        if password: 
+        if password:
             instance.set_password(password)
 
         instance.save()
