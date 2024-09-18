4a936eef313f5c5def9a017d3e93c08b83be8b0d
avalenciasol <avalenciasolano1@gmail.com>
2024-09-18 14:38:02 -0500

    carrito en proceso

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -14,20 +14,20 @@ const Cart = () => {
     },
     {
       id: 2,
-      producto: 'Lijadora Orbital',
-      marca: 'BauKer',
-      precio: 159.900,
+      producto: 'Pintura Interiores - Blanca 5Gl',
+      marca: 'KÖLOR',
+      precio: 224.925,
       cant: 1,
-      imagen: "../../assets/images/products/lijadora-orbital.png"
+      imagen: "../../assets/images/products/pintura.png"
     },
     {
       id: 3,
-      producto: "Lijadora Orbital",
-      marca: "BauKer",
-      precio: 159.920,
+      producto: "Porta Herramientas",
+      marca: "STANLEY",
+      precio: 58.900,
       cant: 1,
-      imagen: "../../assets/images/products/lijadora-orbital.png"
-    } 
+      imagen: "../../assets/images/products/portaHerramientas.png"
+    }
   ])
 
   const incrementarCant = (id) => {
@@ -53,42 +53,46 @@ const Cart = () => {
   const totalAmount = cartItems.reduce((acc, item) => acc + item.precio * item.cant, 0);
 
   return (
-    <div className='cart-container'>
-      <div className='cart-left'>
-        <div className='cart-items'>
-          <h3>Mi carrito ({cartItems.length})</h3>
-          {cartItems.map((item) => (
-            <div className='cart-item'>
-              <img src={item.imagen} alt={item.producto} className='item-image'></img>
-              <div className='item-details'>
-                <h4>{item.producto}</h4>
-                <p>${(item.precio ? (item.precio * item.cant).toFixed(3) : '0')}</p>
-                <div className='item-actions'>
-                  <button onClick={() => decrementCant(item.id)}>-</button>
-                  <span>{item.cant}</span>
-                  <button onClick={() => incrementarCant(item.id)}>+</button>
+    <section className='cart-area'>
+      <div className='cart-wrapper-one'>
+        <div className='cart-container'>
+          <div className='cart-left'>
+            <div className='cart-items'>
+              <h3>Mi carrito ({cartItems.length})</h3>
+              {cartItems.map((item) => (
+                <div className='cart-item'>
+                  <img src={item.imagen} alt={item.producto} className='item-image'></img>
+                  <div className='item-details'>
+                    <h4>{item.producto}</h4>
+                    <p>{(item.precio ? (item.precio * item.cant).toFixed(3) : '0')} COP</p>
+                    <div className='item-actions'>
+                      <button onClick={() => decrementCant(item.id)}>-</button>
+                      <span>{item.cant}</span>
+                      <button onClick={() => incrementarCant(item.id)}>+</button>
+                    </div>
+                    <button className="remove-btn" onClick={() => removeItem(item.id)}>Eliminar</button>
+                  </div>
                 </div>
-                <button className="remove-btn" onClick={() => removeItem(item.id)}>Eliminar</button>
+                ))}
               </div>
             </div>
-            ))}
-          </div>
+            <div className='cart-right'>
+              <div className='direccion'>
+                <h4>Dirección de Envío</h4>
+                <p>Kr 96 C # 22 H - 30</p>
+                <button>Cambiar</button>
+              </div>
+              <div className='resumen-pago'>
+                <h4>Resumen de Pago</h4>
+                <p>Compra = {totalAmount.toFixed(3)} COP</p>
+                <p>Envío = 7.000 COP</p>
+                <h4>Total = {(totalAmount ? (totalAmount + 7.000 ).toFixed(3) : '0')} COP</h4>
+                <button className='orden-btn'>Comprar</button>
+              </div>
+            </div>
         </div>
-        <div className='cart-right'>
-          <div className='direccion'>
-            <h4>Dirección de Envío</h4>
-            <p>Kr 96 C # 22 H - 30</p>
-            <button>Cambiar</button>
-          </div>
-          <div className='resumen-pago'>
-            <h4>Resumen de Pago</h4>
-            <p>Compra: ${totalAmount.toFixed(2)}</p>
-            <p>Envío: 7,000</p>
-            <h4>Total: ${(totalAmount ? (totalAmount - 7000).toFixed(3) : '0')}</h4>
-            <button className='orden-btn'>Comprar</button>
-          </div>
-        </div>
-    </div>
+      </div>
+    </section>
   );
 
 };
